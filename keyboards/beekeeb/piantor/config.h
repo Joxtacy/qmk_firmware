7568b16633e09696043407aec3af47aa441892b7
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-07-03 21:00:50 +0200

    chore(piantor:joxtacy): add some config

--- keyboards/beekeeb/piantor/config.h
+++ keyboards/beekeeb/piantor/config.h
@@ -10,7 +10,7 @@
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
 
-#define BOOTMAGIC_LITE_ROW    0
+#define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 5
 
 // When USB_VBUS_PIN is not defined, SPLIT_USB_DETECT is used.
@@ -18,3 +18,14 @@
 
 // Setting for the TT keycode
 #define TAPPING_TOGGLE 2
+
+// Time in millis to trigger mod (default 200)
+// https://docs.qmk.fm/#/tap_hold?id=tapping-term
+#define TAPPING_TERM 200
+
+// Time in millis between taps to not trigger mod (default TAPPING_TERM)
+// https://docs.qmk.fm/#/tap_hold?id=quick-tap-term
+#define QUICK_TAP_TERM 120
+
+// https://docs.qmk.fm/#/tap_hold?id=permissive-hold
+/* #define PERMISSIVE_HOLD */
